c7b6b48a148ee9c87dffa2f6720c09cd72620c8c
Barroncas1 <128699710+Barroncas1@users.noreply.github.com>
2023-04-24 13:47:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let n1 = Number(prompt('Digite o primeiro número:'))
+let n2 = Number(prompt('Digite o segundo número:'))
+alert(`A soma é ${n1 + n2}`)
+alert(`A subtração é ${n1 - n2}`)
+alert(`A multiplicação é ${n1 * n2}`)
+alert(`A divisão é ${n1 / n2}`)
+alert(`O resto da divisão é ${n1 % n2}`)
