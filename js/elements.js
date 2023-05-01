7545526e587a3311ac1a6070389d222434b51d30
Barroncas1 <128699710+Barroncas1@users.noreply.github.com>
2023-05-01 20:29:10 +0100

    Add files via upload

---
+++ js/elements.js
@@ -0,0 +1,9 @@
+
+export const home = document.querySelector('#home')
+export const universe = document.querySelector('#universe')
+export const explorer = document.querySelector('#explorer')
+export const button = document.querySelector('#universoButton')
+export const body = document.querySelector('body')
+export const div = document.querySelector('#app')
+
+
